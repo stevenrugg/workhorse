c1322d61d4ac5b24cf46adb70fd8b2af8538b97a
Steven Rugg <Steven.rugg@outlook.com>
2023-01-29 09:10:54 +0000

    update project data

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -18,14 +18,14 @@ const projectsData = [
     description:
       'This calculator adds, subtracts, multiplies, divides, roots and provides exponation up to 10^7 are included.  ',
     imgSrc: '/static/images/projects/calculator.jpg',
-    href: 'https://github.com/stevenrugg/calculator.png',
+    href: 'https://github.com/stevenrugg/calculator',
   },
   {
     title: 'Animated Animals',
     description:
       'ES6 JavaScript, CSS3 & HTML5 come together to make a beautiful animation of a bear, world, cat, and deer. Made using SVG segments in the shape of the animal and then mapping the RGB rainbow over the SVG segments .then(animating the whole thing).',
     imgSrc: '/static/images/projects/animatedanimals.png',
-    href: 'https://stevenrugg.github.io/animated-animals.png',
+    href: 'https://stevenrugg.github.io/animated-animals',
   },
 ]
 
