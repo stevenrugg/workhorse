1868b36cdbf61970d6947d0e9914f19b56d7d6f6
Steven Rugg <Steven.rugg@outlook.com>
2023-01-31 10:52:36 -0800

    update

--- pages/api/mailchimp.ts
+++ pages/api/mailchimp.ts
@@ -1,26 +1,42 @@
+import axios from 'axios'
 import { NextApiRequest, NextApiResponse } from 'next'
-import mailchimp from '@mailchimp/mailchimp_marketing'
-
-mailchimp.setConfig({
-  apiKey: process.env.MAILCHIMP_API_KEY,
-  server: process.env.MAILCHIMP_API_SERVER, // E.g. us1
-})
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { email } = req.body
 
-  if (!email) {
+  if (!email || !email.length) {
     return res.status(400).json({ error: 'Email is required' })
   }
 
+  const API_KEY = process.env.MAILCHIMP_API_KEY
+  const API_SERVER = process.env.MAILCHIMP_API_SERVER
+  const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID
+
+  const url = `https://${API_SERVER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`
+
+  const data = {
+    email_address: email,
+    status: 'subscribed',
+  }
+
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `api_key ${API_KEY}`,
+    },
+  }
+
   try {
-    await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
-      email_address: email,
-      status: 'subscribed',
-    })
-    return res.status(201).json({ error: '' })
+    const response = await axios.post(url, data, options)
+    if (response.status >= 400) {
+      return res.status(400).json({
+        error: `There was an error subscribing to the newsletter. Shoot me an email at steven.rugg@zencoder.online and I'll add you to the list.`,
+      })
+    }
+    return res.status(201).json({ message: 'success' })
   } catch (error) {
-    return res.status(500).json({ error: error.message || error.toString() })
+    console.log(error)
+    return res.status(500).json({ error: error.message })
   }
 }
